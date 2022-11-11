d9dc8f35ebe9a64543bdb36acce928de36505b14
nasimovbegzod <begzodnasimov84@gamil.com>
2022-11-11 11:59:59 +0500

    nav tugadi

---
+++ js/main.js
@@ -0,0 +1,54 @@
+function openNavbar() {
+  document.getElementById("navbar-menus").style.left = "0";
+  // document.getElementById("navbar-menus").style.top = "0";
+}
+function closeNavbar() {
+  document.getElementById("navbar-menus").style.left = "-100%";
+  // document.getElementById("navbar-menus").style.top = "-1000%";
+}
+// When the user scrolls down 80px from the top of the document, resize the navbar's padding and the logo's font size
+window.onscroll = function () {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 80 || document.documentElement.scrollTop > 80) {
+    document.getElementById("navbar").className = "navbar-shrink";
+  } else {
+    document.getElementById("navbar").className = "";
+  }
+}
+
+function includeHTML() {
+  var z, i, elmnt, file, xhttp;
+  /* Loop through a collection of all HTML elements: */
+  z = document.getElementsByTagName("*");
+  for (i = 0; i < z.length; i++) {
+    elmnt = z[i];
+    /*search for elements with a certain atrribute:*/
+    file = elmnt.getAttribute("w3-include-html");
+    if (file) {
+      /* Make an HTTP request using the attribute value as the file name: */
+      xhttp = new XMLHttpRequest();
+      xhttp.onreadystatechange = function () {
+        if (this.readyState == 4) {
+          if (this.status == 200) {
+            elmnt.innerHTML = this.responseText;
+          }
+          if (this.status == 404) {
+            elmnt.innerHTML = "Page not found.";
+          }
+          /* Remove the attribute, and call this function once more: */
+          elmnt.removeAttribute("w3-include-html");
+          includeHTML();
+        }
+      };
+      xhttp.open("GET", file, true);
+      xhttp.send();
+      /* Exit the function: */
+      return;
+    }
+  }
+}
+
+includeHTML();
